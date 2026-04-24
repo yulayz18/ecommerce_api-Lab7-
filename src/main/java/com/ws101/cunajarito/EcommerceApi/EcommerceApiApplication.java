09a95a953bc1d22bfaaca5fc4feb496e85fe522e
yulayz18 <yulcedric9@gmail.com>
2026-04-24 08:35:59 +0800

    Task 1: Project Initialization & Structure -> 1.1 Create Spring Boot Project -> 1.2 Verify Project Structure

--- src/main/java/com/ws101/cunajarito/EcommerceApi/EcommerceApiApplication.java
+++ src/main/java/com/ws101/cunajarito/EcommerceApi/EcommerceApiApplication.java
@@ -3,6 +3,11 @@ package com.ws101.cunajarito.EcommerceApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author Yul Cedric Cuna and Julius JaritoMain class for the Ecommerce API application.
+ * This class serves as the entry point for the Spring Boot application.
+ */
 @SpringBootApplication
 public class EcommerceApiApplication {
 
